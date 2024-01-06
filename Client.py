e26326d8f16aee8cbb1b68e8d2d838e52297386e
raz42852 <razderi55@gmail.com>
2024-01-06 14:38:34 +0200

    Commit 2 & add comments

--- Client.py
+++ Client.py
@@ -1,5 +1,4 @@
 import socket
-import Server
 import tkinter as tk
 from tkinter import ttk, filedialog, messagebox
 from tkinterdnd2 import TkinterDnD, DND_FILES
@@ -9,14 +8,25 @@ import time
 
 class SystemFiles():
     def __init__(self, root):
+
         self.root = root
         # Set title
         self.root.title("System Upload - Download files")
 
-        self.file_count = len([item for item in os.listdir(R"C:\Users\raz\Desktop\פרויקטים מועדון המתכנתים\מערכת מאובטחת להעלאת והורדת קבצים\UploadDownloadFilesSecSys\files")])
+        # Initialize var for server and client
+        self.server_host = '127.0.0.1'
+        self.server_port = 12345
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect((self.server_host, self.server_port))
+
+        self.server_path = R"server_data"
+
+        self.files_arr = [item for item in os.listdir(self.server_path)]
+
+        self.file_count = len(self.files_arr)
 
         # Set instruction
-        self.label = ttk.Label(root, text="Select a file ot upload or write the number of the file you want to download : ")
+        self.label = ttk.Label(root, text="Select a file to upload or write the number of the file you want to download/delete : ")
         self.label.pack(pady=10)
 
         # Set file path var
@@ -32,36 +42,48 @@ class SystemFiles():
         self.label_for_num = ttk.Label(root, text="Select the number of the file : ")
         self.label_for_num.pack(pady=10)
 
-        # 
+        # Set field to enter a number for file number
         self.file_num = tk.StringVar()
-        self.file_num_entry = ttk.Entry(root, validate='key')
+        self.file_num_entry = ttk.Entry(root, validate='key', textvariable=self.file_num)
         self.file_num_entry['validatecommand'] = (self.file_num_entry.register(self.validate_entry), '%P')
         self.file_num_entry.pack(pady=10, ipadx=100)
 
-        # Set button to start the scan
-        self.start_upload_button = ttk.Button(root, text="Start Upload", command=self.StartUpload)
+        # Set button to start upload file
+        self.start_upload_button = ttk.Button(root, text="Start Upload", command=lambda: self.StartAction("Upload"))
         self.start_upload_button.pack(pady=10)
 
-        # Set button to start the scan
-        self.start_download_button = ttk.Button(root, text="Start Download", command=self.StartDownload)
+        # Set button to start download file
+        self.start_download_button = ttk.Button(root, text="Start Download", command=lambda: self.StartAction("Download"))
         self.start_download_button.pack(pady=10)
+
+        # Set button to start delete file
+        self.start_delete_button = ttk.Button(root, text="Delete File", command=lambda: self.StartAction("Delete"))
+        self.start_delete_button.pack(pady=10)
+
+        # Set button to show files's server
+        self.start_show_files_button = ttk.Button(root, text="Show Server's Files", command=self.WriteFilesOnScreen)
+        self.start_show_files_button.pack(pady=10)
 
         # Set lable
         self.status_label = ttk.Label(root, text="System status :")
         self.status_label.pack(pady=10)
 
-        # Set area for stuts for every file
+        # Set area for status for every file
         self.text_status = tk.Text(root, height=20, width=100, state="disabled", background="black")
         self.text_status.pack(pady=10)
         self.text_status.tag_configure("defult", foreground="white")
+        self.text_status.tag_configure("title", foreground="green")
+        self.text_status.tag_configure("action", foreground="blue")
 
         # Set quit button
-        self.quit_button = ttk.Button(root, text="Quit", command=root.quit)
+        self.quit_button = ttk.Button(root, text="Quit", command=lambda: self.StartAction("Quit")) # root.quit
         self.quit_button.pack(pady=10)
         
-        # Update the path area if the user choose a file or folder
+        # Update the path area if the user choose a file
         self.root.drop_target_register(DND_FILES)
         self.root.dnd_bind('<<Drop>>', self.UpdateFileEntry)
+
+        self.WriteFilesOnScreen()
 
     def BrowseFile(self):
         # The function ask directory of the file and set it as path
@@ -87,36 +109,94 @@ class SystemFiles():
         file_path = event.data
         self.file_path.set(file_path)
 
-    def StartDownload():
-        pass
+    def WriteOnScreen(self, result, tag):
+        # The function get result and tag that the result is the message and the tag is the color of the message and show it to the user
+        self.text_status.config(state=tk.NORMAL)
+        self.text_status.insert(tk.END, result + "\n\n", tag)
+        self.text_status.config(state=tk.DISABLED)
+        self.text_status.see(tk.END)
 
-    def StartUpload():
-        pass
+    def UpdateFilesArr(self):
+        # The function update the arr of the files and the count by the folder
+        self.files_arr = [item for item in os.listdir(self.server_path)]
+        self.file_count += 1
+
+    def WriteFilesOnScreen(self):
+        # The function write on the screen the files list of the server
+        self.UpdateFilesArr()
+        if len(self.files_arr) == 0:
+            self.WriteOnScreen("Server is empty.", "title")
+        else:
+            self.WriteOnScreen("Files list : ", "title")
+            for i in range(len(self.files_arr)):
+                self.WriteOnScreen(f"{i + 1} - {self.files_arr[i]}", "defult")
 
 
+    def StartAction(self, action):
+        # The function get action and do the action
+        if action == "Delete":
+            # The action send the server request to delete the file that its number is what the user entered
+            if not self.file_num.get() == "":
+                file_name = self.files_arr[int(self.file_num.get()) - 1]
+                self.client_socket.send(f"{action},{file_name}".encode('utf-8'))
+        elif action == "List":
+            # The action send the server action to list all the files
+            self.client_socket.send(f"{action}".encode('utf-8'))
+        elif action == "Download":
+            # The action send the server request to download the file with his action, file name, size and content of the file
+            if not self.file_num.get() == "":
+                file_name = self.files_arr[int(self.file_num.get()) - 1]
+                file_chose_path = os.path.join(self.server_path, file_name)
+                file_size = os.path.getsize(file_chose_path)
 
+                with open(file_chose_path, 'rb') as file:
+                    file_content = file.read()
 
-    def start_client():
-        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server_host = '127.0.0.1'
-        server_port = 12345
+                send_data = f"{action},{file_name},{file_size}"
+                self.client_socket.send(send_data.encode('utf-8'))
+                self.client_socket.sendall(file_content)
 
-        client_socket.connect((server_host, server_port))
-        print(f"Connected to {server_host} : {server_port}")
+        elif action == "Upload":
+            # The action send the server request to upload the file to the server with his action, file name, size and content of the file
+            file_chose_path = self.file_path.get()
+            if not self.file_path.get() == "":
 
-        while True:
-            message = input("Enter your mwssage ('exit' - quit) : ")
-            if message.lower() == 'exit':
-                break
+                file_name = os.path.basename(file_chose_path)
+                file_size = os.path.getsize(file_chose_path)
+                with open(file_chose_path, 'rb') as file:
+                    file_content = file.read()
 
-            client_socket.sendall(message.encode('utf-8'))
-            data = client_socket.recv(1024)
-            ret_message = data.decode('utf-8')
+                send_data = f"{action},{file_name},{file_size}"
+                self.client_socket.send(send_data.encode('utf-8'))
+                self.client_socket.sendall(file_content)
 
-            print(f"Server's answer : {ret_message}")
+        elif action == "Quit":
+            # The action send the server request to close the program
+            self.client_socket.send(action.encode('utf-8'))
+        
+        # Check if the server can send a return message
+        if action == "Upload" and not self.file_path.get() == "" \
+        or action == "Download" and not self.file_num.get() == "" \
+        or action == "Delete" and not self.file_num.get() == "" \
+        or action == "Quit":
+            
+            # Get the data from server and show a message to the user
+            data = self.client_socket.recv(1024).decode('utf-8')
+            cmd, msg = data.split(",")
 
-        print("Lost connection")
-        client_socket.close()
+            if cmd == "DISCONNECTED":
+                self.WriteOnScreen(f"[SERVER]: {msg}", "action")
+                self.client_socket.close()
+                self.root.quit()
+                
+            elif cmd == "ERROR":
+                self.WriteOnScreen(f"[SERVER]: {msg}", "action")
+
+            elif cmd == "OK":
+                self.WriteOnScreen(f"{msg}", "action")
+                self.UpdateFilesArr()
+            self.file_num.set("")
+            self.file_path.set("")
 
 if __name__ == "__main__":
     root = TkinterDnD.Tk()
--- Server.py
+++ Server.py
@@ -1,30 +1,111 @@
 import socket
+import os
+import threading
 
-def start_server():
+# Initialize var for the server and client
+server_host = '127.0.0.1'
+server_port = 12345
+server_path = "server_data"
+client_path = "client_data"
+
+def handle_client(client_socket, client_address):
+    print(f"Connecting with : {client_address}")
+    while True:
+        # Get the action that the client sent
+        data = client_socket.recv(1024).decode('utf-8')
+        data = data.split(",")
+        action = data[0]
+
+        # Check if action is list
+        if action == "List":
+            # Return the client a message include the files details
+            files = os.listdir(server_path)
+            send_data = "OK,"
+
+            if len(files) == 0:
+                send_data += "The server directory is empty"
+            else:
+                send_data += "\n".join(f for f in files)
+            client_socket.send(send_data.encode('utf-8'))
+
+        # Check if action is upload
+        elif action == "Upload":
+            # Get the rest data and upload the file to the server and return to the client appropriate message
+            file_name = data[1]
+            file_size = int(data[2])
+            
+            file_content = b""
+            while len(file_content) < file_size:
+                file_content += client_socket.recv(1024)
+
+            filepath = f"{server_path}\{file_name}"
+            with open(filepath, "wb") as f:
+                f.write(file_content)
+
+            send_data = "OK,File uploaded successfully."
+            client_socket.send(send_data.encode('utf-8'))
+
+        # Check if action is download
+        elif action == "Download":
+            # Get the rest data and download the file to the client folder and return to the client appropriate message
+            file_name = data[1]
+            file_size = int(data[2])
+            file_path = os.path.join(server_path, file_name)
+            destination_path = os.path.join(client_path, file_name)
+
+            if os.path.exists(file_path):
+
+                file_content = b""
+                while len(file_content) < file_size:
+                    file_content += client_socket.recv(1024)
+
+                with open(destination_path, 'wb') as file:
+                    file.write(file_content)
+
+                client_socket.send(f"OK,The file downloaded for client_data folder successfully.".encode('utf-8'))
+
+            else:
+                client_socket.send("ERROR,File not found.".encode('utf-8'))
+
+        # Check if action is deleted
+        elif action == "Delete":
+            # Get the rest data and delete the file from the server and return to the client appropriate message
+            files = os.listdir(server_path)
+            send_data = "OK,"
+            filename = data[1]
+
+            if len(files) == 0:
+                send_data += "The server directory is empty"
+            else:
+                if filename in files:
+                    os.remove(f"{os.getcwd()}\{server_path}\{filename}")
+                    send_data += "File deleted successfully."
+                else:
+                    send_data += "File not found."
+
+            client_socket.send(send_data.encode('utf-8'))
+
+        # Check if action is quit
+        elif action == "Quit":
+            break
+    # Exit
+    client_socket.send(f"DISCONNECTED, Server disconnected".encode('utf-8'))
+    client_socket.close()
+    print(f"[DISCONNECTED] {client_address} disconnected")
+
+def main():
+    print("[STARTING] Server is starting")
+    # Initalize server socket
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_host = '127.0.0.1'
-    server_port = 12345
     server_socket.bind((server_host, server_port))
-    server_socket.listen(1)
-
+    server_socket.listen()
     print(f"Server listen to {server_host} : {server_port}")
 
     while True:
+        # Wait for request and run thread to do the request
         client_socket, client_address = server_socket.accept()
-        print(f"Connecting with : {client_address}")
+        thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
+        thread.start()
 
-        while True:
-            data = client_socket.recv(1024)
-            if not data:
-                break
-            
-            message = data.decode('utf-8')
-            print(f"The message received : {message}")
-
-            client_socket.sendall(data)
-
-        print(f"Lost connection with : {client_address}")
-        client_socket.close()
-
-if __name__ == '__main__':
-    start_server()
+if __name__ == "__main__":
+    main()
